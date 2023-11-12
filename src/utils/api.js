ea9b62c96882a7fc3fbaa99cfcaf0322bdfd9168
SachinMaurya01 <mauryasachin789@gmail.com>
2023-11-12 12:29:43 +0530

    First Commit

---
+++ src/utils/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const BASE_URL = "https://youtube138.p.rapidapi.com";
+
+const options = {
+    params: { hl: "en", gl: "US" },
+    headers: {
+        "X-RapidAPI-Key":"4907c1f1f6msh8ce89ea1cde77c5p10e63djsn337edec8d4de",
+            // process.env.REACT_APP_YOUTUBE_API_KEY || "YOUR_API_KEY",
+        "X-RapidAPI-Host": "youtube138.p.rapidapi.com",
+    },
+};
+
+export const fetchDataFromApi = async (url) => {
+    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+    return data;
+};
